b14cce395ceeea88188027e387a3822efccbdfda
VirajV2000 <virajv2000@gmail.com>
2023-06-16 13:26:14 +0530

    first commit

---
+++ footer.php
@@ -0,0 +1,82 @@
+<footer id="footer" class="footer" style="background:url('images/photo9.jpg');background-repeat:no-repeat;background-size:cover;background:position:center;">
+
+    <div class="footer-content position-relative">
+      <div class="container">
+        <div class="row">
+
+          <div class="col-lg-4 col-md-6">
+            <div class="footer-info">
+              <h3>Event Care</h3>
+              <p>
+                Mangaluru Car Street <br>
+                 575012, India<br><br>
+                <strong>Phone:</strong> 8088759443,8105082398<br>
+                <strong>Email:</strong> eventcare@gmail.com<br>
+              </p>
+              <div class="social-links d-flex mt-3">
+                <a href="#" class="d-flex align-items-center justify-content-center"><i class="bi bi-twitter"></i></a>
+                <a href="#" class="d-flex align-items-center justify-content-center"><i class="bi bi-facebook"></i></a>
+                <a href="#" class="d-flex align-items-center justify-content-center"><i class="bi bi-instagram"></i></a>
+                <a href="#" class="d-flex align-items-center justify-content-center"><i class="bi bi-linkedin"></i></a>
+              </div>
+            </div>
+          </div><!-- End footer info column-->
+
+          <div class="col-lg-2 col-md-3 footer-links">
+            <h4>Useful Links</h4>
+            <ul>
+              <li><a href="#">Home</a></li>
+              <li><a href="#">About us</a></li>
+              <li><a href="#">Services</a></li>
+              <li><a href="#">Terms of service</a></li>
+              <li><a href="#">Privacy policy</a></li>
+            </ul>
+          </div><!-- End footer links column-->
+
+          <div class="col-lg-2 col-md-3 footer-links">
+            <h4>Our Events</h4>
+            <ul>
+              <li><a href="#">Wedding</a></li>
+              <li><a href="#">Birthday</a></li>
+              <li><a href="#">Baby Shower</a></li>
+              <li><a href="#">Engagement</a></li>
+              <li><a href="#">Concert</a></li>
+            </ul>
+          </div><!-- End footer links column-->
+
+          <div class="col-lg-2 col-md-3 footer-links">
+            <h4>Our Services</h4>
+            <ul>
+              <li><a href="#">Catering</a></li>
+              <li><a href="#">Photography</a></li>
+              <li><a href="#">Decoration</a></li>
+              <li><a href="#">DJ</a></li>
+              <li><a href="#">Lighting</a></li>
+            </ul>
+          </div><!-- End footer links column-->
+
+          <div class="col-lg-2 col-md-3 footer-links">
+            <h4>Event Managers</h4>
+            <ul>
+              <li><a href="#">Viraj Raj</a></li>
+              <li><a href="#">Manish Kotian</a></li>
+              <li><a href="#">Sagar Shetty</a></li>
+              <li><a href="#">Kavya Theertha</a></li>
+              <li><a href="#">Deekshith Shiva</a></li>
+            </ul>
+          </div><!-- End footer links column-->
+
+        </div>
+      </div>
+    </div>
+
+    <div class="footer-legal text-center position-relative">
+      <div class="container">
+        <div class="copyright">
+          &copy; Copyright <strong><span>EventCare</span></strong>. All Rights Reserved
+        </div>
+       
+      </div>
+    </div>
+
+  </footer>
---
+++ header.php
@@ -0,0 +1,31 @@
+<!-- ======= Header ======= -->
+<header id="header" class="header d-flex align-items-center">
+    <div class="container-fluid container-xl d-flex align-items-center justify-content-between">
+
+      <a href="index.html" class="logo d-flex align-items-center">
+        <!-- Uncomment the line below if you also wish to use an image logo -->
+        <!-- <img src="assets/img/logo.png" alt=""> -->
+        <h1>Event Care<span>.</span></h1>
+      </a>
+
+      <i class="mobile-nav-toggle mobile-nav-show bi bi-list"></i>
+      <i class="mobile-nav-toggle mobile-nav-hide d-none bi bi-x"></i>
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><a href="index.php" class="active">Home</a></li>
+          <li><a href="#about">About</a></li>
+          <li><a href="#services">Services</a></li>
+          <li><a href="#gallery">Gallery</a></li>
+       
+          <li class="dropdown"><a href=""><span>Login</span></a>
+        <ul>
+        <li><a href="customer/index.php">Customer</a></li>
+        <li><a href="event_manager/index.php">Event Manager</a></li>
+        </ul>
+        </li>
+          <li><a href="#contact">Contact</a></li>
+        </ul>
+      </nav><!-- .navbar -->
+
+    </div>
+  </header><!-- End Header -->
